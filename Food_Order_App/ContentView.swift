0f4fa28dd9640b709dd0172473363134aa0d8c9f
Aamir Khan <oneaamirkhan@icloud.com>
2023-11-20 17:29:13 +0530

    Initial Commit

---
+++ Food_Order_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food_Order_App
+//
+//  Created by عامر خان on 20/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Food_Order_App/Food_Order_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Order_AppApp.swift
+//  Food_Order_App
+//
+//  Created by عامر خان on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Food_Order_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
